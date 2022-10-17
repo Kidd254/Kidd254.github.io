8ef7f425cdd8c1f45a4c06a01ebef2e1aeaf55f5
mastercoder <lorenkioko@gmail.com>
2022-10-17 21:52:10 +0300

    js file modified

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 function reveal() {
-  document.getElementById('image').style.display ='block';
-  document.getElementById('btn').style.display ='none';
-  onclick = "this.style.display='none'";
+  document.getElementById('image').style.display = 'block';
+  document.getElementById('btn').style.display = 'none';
 }
 reveal();
